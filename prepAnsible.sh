d212433dedefbf09a84bd2d3d0e6f1307244d8e2
kaiAsmOne <kai.thorsrud@gmail.com>
2021-01-19 18:08:04 +0100

    updated cfg

--- prepAnsible.sh
+++ prepAnsible.sh
@@ -4,20 +4,21 @@ echo "start"
 cd /home/sicraroot
 
 echo "Installing gitclient"
-sudo dnf install git
+sudo dnf install git -y
 echo "Installing Python 3"
-sudo dnf install python3
+sudo dnf install python3 -y
 echo "Installing Python 3 PIP"
-sudo dnf install python3-pip
+sudo dnf install python3-pip -y
 echo "Installing Python 3 Virtual Env"
 sudo -H python3 -m pip install virtualenv
 echo "Installing Python 3 Packaging"
-sudo python3 -m pip install packaging
+python3 -m pip install packaging
 echo "Installing Python 3 MS RestAzure"
-sudo python3 -m pip install msrestazure
+python3 -m pip install msrestazure
 echo "Installing from Git"
 curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 sudo python3 -m pip install -r requirements-azure.txt
+sudo python3 -m pip install ansible[azure]
 rm requirements-azure.txt
 ansible-galaxy collection install azure.azcollection
 echo "config done"
