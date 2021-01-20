694ccac580915b44664e54efab3f11c76514cefa
kaiAsmOne <kai.thorsrud@gmail.com>
2021-01-20 03:50:16 +0100

    Order of xcute

--- prepAnsible.sh
+++ prepAnsible.sh
@@ -12,21 +12,21 @@ sudo dnf install python3 -y
 echo "Installing Python 3 PIP"
 sudo dnf install python3-pip -y
 echo "Installing Python 3 Virtual Env"
-python3 -m pip install virtualenv
+sudo python3 -m pip install virtualenv
 echo "Installing Python 3 Packaging"
-python3 -m pip install packaging
+sudo python3 -m pip install packaging
 echo "Installing Python 3 MS RestAzure"
-python3 -m pip install msrestazure
+sudo python3 -m pip install msrestazure
 echo "Installing python3-devel"
 sudo dnf install gcc python3-devel -y
-echo "Installing Ansible Azure Modules"
-curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
-python3 -m pip install -r requirements-azure.txt
 echo "Installing Ansible"
-python3 -m pip install ansible[azure]
+sudo python3 -m pip install ansible
 echo "Installing Azure CLI"
-python3 -m pip install azure-cli
+sudo python3 -m pip install azure-cli
 echo "Installing Ansible Azure Modules"
-ansible-galaxy collection install azure.azcollection
+sudo curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo python3 -m pip install -r requirements-azure.txt
+echo "Installing Ansible Azure Modules"
+sudo ansible-galaxy collection install azure.azcollection
 echo "Config done"
 exit 0
