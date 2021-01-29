98bf0ef2252f3b04de5f1d707eec086bb61a27e7
kaiAsmOne <kai.thorsrud@gmail.com>
2021-01-29 14:18:24 +0100

    Change Script

--- prepAnsiblevar.sh
+++ prepAnsiblevar.sh
@@ -25,6 +25,7 @@ sudo python3 -m pip install -r requirements-azure.txt
 echo "Installing Ansible Azure Modules"
 sudo ansible-galaxy collection install azure.azcollection
 echo "Login with Azure CLI"
+echo "sudo -S -u sicraroot -i /bin/bash -l az login --service-principal -u $1 -p $2 --tenant $3" > /home/sicraroot/var.txt
 sudo -S -u sicraroot -i /bin/bash -l az login --service-principal -u $1 -p $2 --tenant $3
 echo "Config done"
 exit 0
