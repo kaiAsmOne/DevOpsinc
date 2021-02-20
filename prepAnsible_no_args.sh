48f5f1d6e69642c5171a550b0927dde6d30607a1
kaiAsmOne <kai.thorsrud@gmail.com>
2021-02-20 15:09:55 +0100

    Fixed Azure Storage Packages

--- prepAnsible_no_args.sh
+++ prepAnsible_no_args.sh
@@ -38,22 +38,18 @@ sudo dnf install python3 -y
 echo "Installing Python 3 PIP" >> /home/$4/installstatus.txt
 sudo dnf install python3-pip -y
 echo "Installing Python 3 Virtual Env" >> /home/$4/installstatus.txt
-# Mangla rettigheter allerede på pakka under her
 sudo python3 -m pip install virtualenv
 echo "Installing python3-devel" >> /home/$4/installstatus.txt
 sudo dnf install gcc python3-devel -y
 echo "Installing Ansible" >> /home/$4/installstatus.txt
 /bin/su - sicraroot -c  "python3 -m pip install --user ansible"
-echo "Installing Azure CLI" >> /home/$4/installstatus.txt
-/bin/su - sicraroot -c  "python3 -m pip install --user azure-cli"
 echo "Installing Ansible Azure Modules" >> /home/$4/installstatus.txt
-curl -O https://raw.githubusercontent.com/Azure/azure_preview_modules/master/files/requirements-azure.txt
+curl -O https://raw.githubusercontent.com/kaiAsmOne/DevOpsinc/main/requirements-azure.txt
 #sudo curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 #sudo python3 -m pip install -r requirements-azure.txt
-/bin/su - sicraroot -c  "python3 -m pip install --user -r requirements-azure.txt"
+/bin/su - sicraroot -c  "python3 -m pip install --user -r /home/sicraroot/requirements-azure.txt"
 
 echo "Installing Ansible Azure Modules" >> /home/$4/installstatus.txt
-# Kommando under må kjøres som aktuell bruker
 /bin/su - sicraroot -c  "ansible-galaxy collection install azure.azcollection"
 echo "Login with Azure CLI" >> /home/$4/installstatus.txt
 sudo /bin/su -c "/usr/local/bin/az login --service-principal -u $1 -p $2 --tenant $3" - $4
