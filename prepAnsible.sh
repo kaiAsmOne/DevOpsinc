6582a81b06f3f5752969b02be9fcdb0721811aca
kaiAsmOne <kai.thorsrud@gmail.com>
2021-02-25 17:08:37 +0100

    Revision 0.2

--- prepAnsible.sh
+++ prepAnsible.sh
@@ -5,7 +5,7 @@ echo "Ansible Deploy Machine for Azure Piplelines"
 echo "By Kai Thorsrud, Sicra A/S https://sicra.no/ "
 echo "---------------------------------------------"
 
-# This script takes 7 parameters
+# This script takes 9 parameters
 # 1= SPN URL to use for az login
 # 2= Secret of SPN above
 # 3= Tenant to login to with az login
@@ -44,13 +44,11 @@ sudo dnf install gcc python3-devel -y
 echo "Installing Ansible" >> /home/$4/installstatus.txt
 sudo python3 -m pip install ansible
 echo "Installing Azure CLI" >> /home/$4/installstatus.txt
-sudo python3 -m pip install azure-cli
+sudo python3 -m pip install azure-cli==2.11.1
 echo "Installing Ansible Azure Modules" >> /home/$4/installstatus.txt
-curl -O https://github.com/Azure/azure_preview_modules/blob/master/files/requirements-azure.txt
-#sudo curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo curl -O https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
 sudo python3 -m pip install -r requirements-azure.txt
 echo "Installing Ansible Azure Modules" >> /home/$4/installstatus.txt
-# Kommando under må kjøres som aktuell bruker
 sudo ansible-galaxy collection install azure.azcollection
 echo "Login with Azure CLI" >> /home/$4/installstatus.txt
 sudo /bin/su -c "/usr/local/bin/az login --service-principal -u $1 -p $2 --tenant $3" - $4
@@ -78,19 +76,17 @@ sudo ./svc.sh install $4
 echo "Start DevOpsAgent" >> /home/$4/installstatus.txt
 sudo ./svc.sh start
 echo "service started" >> /home/$4/installstatus.txt
+echo "Install Azure CLI That works with both Ansible and az cli commands. Double install due to bug" >> /home/$4/installstatus.txt
+sudo python3 -m pip install azure-cli==2.11.1
 echo "Create gcp folder" >> /home/$4/installstatus.txt
-sudo python3 -m pip install azure-cli
 cd /home/$4
 mkdir gcp
 chown -R $4:$4 gcp/
 cd gcp
 echo "Create gcp JSON" >> /home/$4/installstatus.txt
-/bin/su - $4 -c "az keyvault secret show --name '$8' --vault-name '$9' --query 'value' > /home/$4/gcp/gcpcredz.json"
+/bin/su - $4 -c "az keyvault secret download --name '$8' --vault-name '$9' -f /home/$4/gcp/gcpcredz.json"
 echo "Set GCP Variables" >> /home/$4/installstatus.txt
-#/bin/su - $4 -c "export GCP_AUTH_KIND=serviceaccount"
-#/bin/su - $4 -c "export GCP_SERVICE_ACCOUNT_FILE=/home/$4/gcp/gcpcredz.json"
 echo "export GCP_SERVICE_ACCOUNT_FILE=/home/'$4'/gcp/gcpcredz.json" >> /home/$4/.bashrc
 echo "export GCP_AUTH_KIND=serviceaccount" >> /home/$4/.bashrc
-
 echo "config done" >> /home/$4/installstatus.txt
 exit 0
