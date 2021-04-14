39bcdf01694436e09c15cded95aaf22524328b07
kaiAsmOne <kai.thorsrud@gmail.com>
2021-04-14 10:20:45 +0200

    Finalize

--- prepAnsiblevar.sh
+++ prepAnsiblevar.sh
@@ -4,7 +4,6 @@ echo "---------------------------------------------"
 echo "Ansible Deploy Machine for Azure Piplelines"
 echo "By Kai Thorsrud, Sicra A/S https://sicra.no/ "
 echo "---------------------------------------------"
-echo "Start Config Job" > /home/$4/installstatus.txt
 sudo dnf install git -y
 sudo dnf install python3 -y
 sudo dnf install python3-pip -y
@@ -22,7 +21,6 @@ mkdir agent
 cd agent
 AGENTRELEASE="$(curl -s https://api.github.com/repos/Microsoft/azure-pipelines-agent/releases/latest | grep -oP '"tag_name": "v\K(.*)(?=")')"
 AGENTURL="https://vstsagentpackage.azureedge.net/agent/${AGENTRELEASE}/vsts-agent-linux-x64-${AGENTRELEASE}.tar.gz"
-echo "Release "${AGENTRELEASE}" appears to be latest" >> /home/$4/installstatus.txt
 wget -O agent.tar.gz ${AGENTURL} 
 tar zxvf agent.tar.gz
 chmod -R 777 .
@@ -30,18 +28,14 @@ sudo /bin/su -c "/home/$4/agent/bin/installdependencies.sh"
 /bin/su -c "/home/$4/agent/config.sh --unattended --url '$5' --auth pat --token '$6' --pool '$7' --agent $HOSTNAME --acceptTeeEula --work ./_work --runAsService --acceptTeeEula --replace" - $4
 sudo ./svc.sh install $4
 sudo ./svc.sh start
-echo "service started" >> /home/$4/installstatus.txt
-echo "Install Azure CLI That works with both Ansible and az cli commands. Double install due to bug" >> /home/$4/installstatus.txt
+echo "Install Azure CLI That works with both Ansible and az cli commands. Double install due to bug"
 sudo python3 -m pip install azure-cli==2.11.1
 cd /home/$4
 mkdir gcp
 chown -R $4:$4 gcp/
 cd gcp
-echo "Create gcp JSON" >> /home/$4/installstatus.txt
 /bin/su - $4 -c "az keyvault secret download --name '$8' --vault-name '$9' -f /home/$4/gcp/gcpcredz.json"
-echo "Set Variables" >> /home/$4/installstatus.txt
 echo "export GCP_SERVICE_ACCOUNT_FILE=/home/'$4'/gcp/gcpcredz.json" >> /home/$4/.bashrc
 echo "export GCP_AUTH_KIND=serviceaccount" >> /home/$4/.bashrc
 echo "export ANSIBLE_HOST_KEY_CHECKING=False" >> /home/$4/.bashrc
-echo "config done" >> /home/$4/installstatus.txt
 exit 0
